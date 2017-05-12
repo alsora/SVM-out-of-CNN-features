c096d2afe0b3e212e15b13354f2e81d9b2a3174b
alberto soragna <alberto.soragana@gmail.com>
2017-05-12 18:28:17 +0200

    structure completed. it works on pascal voc 2007

--- CNN_SVM_main.py
+++ CNN_SVM_main.py
@@ -5,6 +5,7 @@ import caffe
 # caffe.set_device(GPU_ID)
 from datetime import datetime
 import numpy as np
+import matplotlib.pyplot as plt
 import sys, getopt
 import cv2
 import os
@@ -22,28 +23,33 @@ from compute_mean import compute_mean
 
 
 
-
 def vis_square(data):
     """Take an array of shape (n, height, width) or (n, height, width, 3)
        and visualize each (height, width) thing in a grid of size approx. sqrt(n) by sqrt(n)"""
-    
+    print data.shape
     # normalize data for display
     data = (data - data.min()) / (data.max() - data.min())
-    
+        
+    print data.shape
+
     # force the number of filters to be square
     n = int(np.ceil(np.sqrt(data.shape[0])))
     padding = (((0, n ** 2 - data.shape[0]),
                (0, 1), (0, 1))                 # add some space between filters
                + ((0, 0),) * (data.ndim - 3))  # don't pad the last dimension (if there is one)
     data = np.pad(data, padding, mode='constant', constant_values=1)  # pad with ones (white)
-    
+    print data.shape
+
     # tile the filters into an image
     data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3) + tuple(range(4, data.ndim + 1)))
     data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
     
+    print data.shape
+
     plt.imshow(data); plt.axis('off')
 
 	#cv2.waitKey(0)
+
 
 
 def splitDataset(imgSetName, trainPercentage):
@@ -78,6 +84,9 @@ def splitDataset(imgSetName, trainPercentage):
 			testSet.append(imgSet[i])
 			countTest += 1
 
+	random.shuffle(trainSet)
+	random.shuffle(testSet)
+
 	trainFile = open(imagesTrainSet, "w")
 	trainFile.write("\n".join(trainSet))
 	trainFile.close()
@@ -85,11 +94,10 @@ def splitDataset(imgSetName, trainPercentage):
 	testFile = open(imagesTestSet, "w")
 	testFile.write("\n".join(testSet))
 	testFile.close()
-	#print trainSet
-	#print len(trainSet)
+
     
 
-	return [trainSet, testSet]
+	return [imagesTrainSet, imagesTestSet]
 
 
 
@@ -112,9 +120,8 @@ def createSamplesDatastructures(samplesListFileName, interesting_labels):
 			samplesImages.append(image)
 
 			annotationCompletePath = annotationsFolderPath + sampleName + '.xml'
-			labels = readLabelFromAnnotation(annotationCompletePath, interesting_labels)
-			print "the label of image %s is %s" % (sampleName, labels)
-			samplesLabels.append(labels)
+			label = readLabelFromAnnotation(annotationCompletePath, interesting_labels)
+			samplesLabels.append(label)
 
 	return [samplesNames, samplesImages, samplesLabels]
 
@@ -123,24 +130,18 @@ def createSamplesDatastructures(samplesListFileName, interesting_labels):
 def readLabelFromAnnotation(annotationFileName, interesting_labels):
 	#Parse the given annotation file and read the label
 
-	all_labels = []
-	labels = []
+	label = []
 	tree = ET.parse(annotationFileName)
 	root = tree.getroot()
 	for obj in root.findall('object'):
 		name = obj.find('name').text
-		#print name
-		all_labels.append(name)
 
-	# returns all interesting labels found in an image
-	labels = [x for x in interesting_labels if x in all_labels]
+		if (name in interesting_labels):
+			label.append(name)
+			break
 
-	# if there are more labels in an image pick one randomly
-	if len(labels) > 1:
-		#labels = random.choice(labels)
-		labels = labels[1]  
 
-	return labels
+	return label
 
 
 
@@ -152,7 +153,7 @@ def extractFeatures(imageSet, net, extractionLayerName):
 		net.blobs['data'].data[...] = image
 		net.forward()
 		features = net.blobs[extractionLayerName].data[0]
-		featuresVector.append(features.flatten())
+		featuresVector.append(features.copy().flatten())
 
 	return featuresVector
 
@@ -183,7 +184,7 @@ def main(argv):
 
 	print 'model file is "', model_filename
 	print 'weight file is "', weight_filename
-	#print 'image file is "', listImagesNames
+	print 'image file is "', listImagesNames
 
 
 	caffe.set_mode_cpu()
@@ -192,25 +193,100 @@ def main(argv):
 	    print 'Caffe model found.'
 	else:
 	    print 'Caffe model NOT found...'
-	    #sys.exit(2)
+	    sys.exit(2)
 
-
-	AllImagesSet = "ImagesSet.txt"
-	SampleImage = "ImageTestForAnnotation.txt"
 	trainDataPercentage = 0.7
 	
 	interesting_labels = ['aeroplane','bird','cat','boat','horse']
-	# se per esempio qua aggiungi 'person' vedi che ti stampa anche quel label
+	trainFeaturesFileName = 'trainFeatures.b'
+	testFeaturesFileName =  'testFeatures.b'
+
 
 	#Given file listImagesNames and percentage-> listImagesNamesTrain listImagesNamesTest
-	#[listImagesNamesTrain, listImagesNamesTest] = splitDataset(AllImagesSet, trainDataPercentage)
-	#print (listImagesNamesTrain)
-	[A,B,Labels] = createSamplesDatastructures(AllImagesSet, interesting_labels)
-	#print Labels
-	#print len(Labels)
+	[listImagesNamesTrain, listImagesNamesTest] = splitDataset(listImagesNames, trainDataPercentage)
 
 
 
+	#Load all images in data structure -> imagesTrain imagesTest
+	[filesTrainNames, imagesTrain, labelsTrain] = createSamplesDatastructures(listImagesNamesTrain, interesting_labels)
+	[filesTestNames, imagesTest, labelsTest] = createSamplesDatastructures(listImagesNamesTest, interesting_labels)
+
+
+	#CNN creation
+	net = caffe.Net(model_filename,      # defines the structure of the model
+	               weight_filename,  # contains the trained weights
+	              caffe.TEST)     # use test mode (e.g., don't perform dropout)
+
+
+	extractionLayerName = 'fc25'
+	if extractionLayerName not in net.blobs:
+		raise TypeError("Invalid layer name: " + extractionLayerName)
+
+	transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+	transformer.set_transpose('data', (2,0,1)) #move image channels to outermost dimension 
+
+
+
+	#Update the sets of images by transforming them according to Transformer
+	for  index in range(len(imagesTrain)):
+		imagesTrain[index] = transformer.preprocess('data', imagesTrain[index])
+
+	for  index in range(len(imagesTest)):
+		imagesTest[index] = transformer.preprocess('data', imagesTest[index])		
+
+
+	#Forward the net on all images -> featureVectorsTrain featureVectorsTest
+	featureVectorsTrain = []
+	featureVectorsTest = []
+	
+	t1 = time.time()
+	featureVectorsTrain = extractFeatures(imagesTrain, net, extractionLayerName)
+	featureVectorsTest = extractFeatures(imagesTest, net, extractionLayerName)
+	print 'Features extraction took ',(time.time() - t1) ,' seconds for ', (len(imagesTrain) + len(imagesTest)), ' images'
+
+	#Dump features in 2 files -> trainFeatures testFeatures
+	with open(trainFeaturesFileName, 'wb') as trainFeaturesFile:
+		pickle.dump((filesTrainNames, featureVectorsTrain), trainFeaturesFile)
+
+	with open(testFeaturesFileName, 'wb') as testFeaturesFile:
+		pickle.dump((filesTestNames, featureVectorsTest), testFeaturesFile)	
+
+
+	#Load features from a previously dumped file
+	with open(trainFeaturesFileName, 'rb') as trainFeaturesFile:
+		(filesTrainNames, featureVectorsTrain) = pickle.load(trainFeaturesFile)
+		featureVectorsTrain = np.array(featureVectorsTrain)
+
+	with open(testFeaturesFileName, 'rb') as testFeaturesFile:
+		(filesTestNames, featureVectorsTest) = pickle.load(testFeaturesFile)
+		featureVectorsTest = np.array(featureVectorsTest)
+
+
+	#Fit a SVM model on the extracted trainFeatures
+	t1 = time.time()
+	modelSVM = svm.SVC(kernel="linear", C=1e6, probability=True) # little regularization needed - get this training set right, neglect margin
+	modelSVM.fit(featureVectorsTrain, labelsTrain)
+	print 'SVM training took ',(time.time() - t1) ,' seconds'
+
+
+
+	#Test the SVM using the extracted testFeatures
+	t1 = time.time()
+	for index in range(len(filesTestNames)):
+		features =  np.array(featureVectorsTest[index]).reshape((1, -1))
+		prediction = modelSVM.predict(features)
+		print 'For image ', filesTestNames[index], ' ... Predicted: ', prediction, ' TrueLabel: ', labelsTest[index]
+
+
+	print 'SVM test took ',(time.time() - t1) ,' seconds'
+
+
+	'''
+	filters = net.params['conv1'][0].data
+	vis_square(filters.transpose(0, 2, 3, 1))
+
+	vis_square(featureVectorsTest[0])
+	'''
 
 if __name__=='__main__':	
 	main(sys.argv[1:])
