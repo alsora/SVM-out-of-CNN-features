574adc0056ad453bc55551f0229701b283065a9e
alberto soragna <alberto.soragana@gmail.com>
2017-05-16 13:44:40 +0200

    modified get_path.py: now it takes as argument the number of images

--- get_path.py
+++ get_path.py
@@ -32,10 +32,19 @@ def writeImages(num,classes):
 
 
 def main(argv):
+
+
+	num = 0
+
+	if len(argv) == 1:
+		num = int(argv[0])
+	else:
+		print 'Provide the number of images !!!!!!!', 
+		sys.exit(2)
 	
 	interesting_labels = ['n02117135','n02129604','n02134084','n02398521','n02481103']
 
-	writeImages(5,interesting_labels)
+	writeImages(num,interesting_labels)
 
 
 
