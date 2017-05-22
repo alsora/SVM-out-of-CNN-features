a6607a26ebd13f8635d1435cc96bb9c618b63206
alberto soragna <alberto.soragana@gmail.com>
2017-05-22 10:16:38 +0200

    implemented training procedure from cropped images for the whole pascal voc dataset

--- CNN_SVM_detection.py
+++ CNN_SVM_detection.py
@@ -1,37 +1,235 @@
+# set up Python environment: numpy for numerical routines, and matplotlib for plotting
+import os
+os.environ['GLOG_minloglevel'] = '3' 
+
+import caffe
+import itertools
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import sys, getopt
+import cv2
+import pickle
+from sklearn import svm
+import time
+import random
+import xml.etree.ElementTree as ET
+import hashlib
+import glob
+from sklearn.metrics import confusion_matrix
+
+from caffe.io import array_to_blobproto
+from collections import defaultdict
+from skimage import io
+
+from compute_mean import compute_mean 
+
+
+
+
+
+
+
+def createSamplesDatastructures(images_dir, annotations_dir, mode):
+
+
+    samplesNames = []
+    samplesImages = []
+    samplesLabels = []
+
+
+    if mode == 'voc':
+
+        for root, dirs, files in os.walk(images_dir):
+            for image_name in files:
+                name, extension = image_name.split(".")
+
+                samplesNames.append(name)
+
+                imageCompletePath = images_dir + '/' + image_name
+                image = caffe.io.load_image(imageCompletePath)
+                samplesImages.append(image)
+
+                annotationCompletePath = annotations_dir + '/' + name + '.xml'
+                label = readLabelFromAnnotation(annotationCompletePath)
+                samplesLabels.append(label)
+
+        imagesFolderPath = images_dir
+        annotationsFolderPath = annotations_dir
+
+
+        return [samplesNames, samplesImages, samplesLabels]
+
+
+
+
+def trainSVMsFromCroppedImages(net, extractionLayerName, images_dir, annotations_dir):
+
+    [filesTrainNames, imagesTrain, labelsTrain] = createSamplesDatastructures(images_dir, annotations_dir, 'voc')
+
+    trainFeaturesFileName = 'trainFeatures.b'
+
+    if not os.path.isfile(trainFeaturesFileName):
+
+        #YOLO CNN uses images in range [0,1], while other models (VGG, faster RCNN ...) use images in range [0,255]
+        '''if 'yolo' not in model_filename:
+            imagesScale = 1.0;
+        else:
+            imagesScale = 255.0
+
+'''
+        imagesScale = 255.0
+
+        transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+        transformer.set_transpose('data', (2,0,1)) #move image channels to outermost dimension 
+        transformer.set_raw_scale('data', imagesScale) 
+
+        #Update the sets of images by transforming them according to Transformer
+        for  index in range(len(imagesTrain)):
+            imagesTrain[index] = transformer.preprocess('data', imagesTrain[index])
+
+        featureVectorsTrain = []
+
+        t1 = time.time()
+        featureVectorsTrain = extractFeatures(imagesTrain, net, extractionLayerName)
+        print 'Features extraction took ',(time.time() - t1) ,' seconds for ', len(imagesTrain), ' images'
+
+        #Dump features in a file 
+        with open(trainFeaturesFileName, 'wb') as trainFeaturesFile:
+            pickle.dump((filesTrainNames, featureVectorsTrain), trainFeaturesFile)
+
+    else:
+
+        print 'Opening old features.... '
+        #Load features from a previously dumped file
+        with open(trainFeaturesFileName, 'rb') as trainFeaturesFile:
+            (filesTrainNames, featureVectorsTrain) = pickle.load(trainFeaturesFile)
+            featureVectorsTrain = np.array(featureVectorsTrain)
+
+
+    featureVectorsTrainNormalized = []
+
+    for vec in featureVectorsTrain:
+        vecNormalized = vec/np.linalg.norm(vec)
+        featureVectorsTrainNormalized.append(vecNormalized)
+
+    trainMean = np.mean(featureVectorsTrainNormalized, axis = 0)
+
+    featureVectorsTrainNormalizedCentered = []
+
+    for vec in featureVectorsTrainNormalized:
+        vecCentered = vec - trainMean
+        featureVectorsTrainNormalizedCentered.append(vecCentered)
+
+
+    #Creates and train 2 svms: a novelty detector and a multi class classifier one
+    noveltySVM = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
+    multiclassSVM = svm.SVC(kernel="rbf", C=1e6, probability=True) # little regularization needed - get this training set right, neglect margin
+
+    t1 = time.time()
+    noveltySVM.fit(featureVectorsTrainNormalizedCentered)
+    multiclassSVM.fit(featureVectorsTrainNormalizedCentered, labelsTrain)
+    print 'SVMs training took ',(time.time() - t1) ,' seconds'
+
+
+
+    return [noveltySVM, multiclassSVM]
+
+
+
+
+
+
+def readLabelFromAnnotation(annotationFileName):
+    #Parse the given annotation file and read the label
+
+    tree = ET.parse(annotationFileName)
+    root = tree.getroot()
+    for obj in root.findall('object'):
+        name = obj.find('name').text
+
+        return name
+
+
+def extractFeatures(imageSet, net, extractionLayerName):
+
+    featuresVector = []
+
+    for image in imageSet:
+        net.blobs['data'].data[...] = image
+        net.forward()
+        features = net.blobs[extractionLayerName].data[0]
+        featuresVector.append(features.copy().flatten())
+
+    return featuresVector
+
+
+def plot_confusion_matrix(cm, classes,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    np.set_printoptions(precision=2)
+
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, np.around(cm[i, j], decimals=2),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    #print(cm)
+
+    plt.tight_layout()
+    plt.ylabel('True labels')
+    plt.xlabel('Predicted labels')
+    plt.savefig("confusion_matrix.png")
+
+
+
+
 def main(argv):
 
     model_filename = ''
     weight_filename = ''
-    img_filename = ''
-    mode = ' '
+    images_dir = ''
+    annotations_dir = ''
     try:
-        opts, args = getopt.getopt(argv, "hm:w:i:s:")
+        opts, args = getopt.getopt(argv, "hm:w:i:a:")
         print opts
     except getopt.GetoptError:
-        print 'CNN_SVM_main.py -m <model_file> -w <weight_file> -i <img_files_list> -s <mode>'
+        print 'CNN_SVM_main.py -m <model_file> -w <weight_file> -i <images_dir> -a <annotations_dir>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'CNN_SVM_main.py -m <model_file> -w <weight_file> -i <img_files_list> -s <mode>'
+            print 'CNN_SVM_main.py -m <model_file> -w <weight_file> -i <images_dir> -s <annotations_dir>'
             sys.exit()
         elif opt == "-m":
             model_filename = arg
         elif opt == "-w":
             weight_filename = arg
         elif opt == "-i":
-            listImagesNames = arg
-        elif opt == "-s":
-            mode = arg;
+            images_dir = arg
+        elif opt == "-a":
+            annotations_dir = arg;
 
     print 'model file is ', model_filename
     print 'weight file is ', weight_filename
-    print 'image file is ', listImagesNames
-    print 'mode is ', mode
+    print 'images dir is ', images_dir
+    print 'annotations dir is ', annotations_dir
 
 
 
     interesting_labels = ['aeroplane','bird','cat','boat','horse']
-    trainDataPercentage = 0.7
 
 
     if os.path.isfile(model_filename):
@@ -40,10 +238,6 @@ def main(argv):
         print 'Caffe model NOT found...'
         sys.exit(2)
 
-
-    if not mode == 'voc' and not mode == 'imagenet':
-        print 'The given mode is not supported...'
-        sys.exit(2)
 
     caffe.set_mode_cpu()
 
@@ -58,169 +252,8 @@ def main(argv):
         raise TypeError("Network " +model_filename + " does not contain layer with name: " + extractionLayerName)
 
 
-
-
-    md5CheckSum = hashlib.md5(open(listImagesNames,'rb').read()).hexdigest()
-    partialMd5CheckSum = md5CheckSum[:5]
-
-    # Import the cropped images to be forwarded to the net
-
-
-    #Given file listImagesNames and percentage-> listImagesNamesTrain listImagesNamesTest
-    [listImagesNamesTrain, listImagesNamesTest] = splitDataset(listImagesNames, trainDataPercentage, partialMd5CheckSum)
-
-
-    #Load all images in data structure -> imagesTrain imagesTest
-    [filesTrainNames, imagesTrain, labelsTrain] = createSamplesDatastructures(listImagesNamesTrain, interesting_labels, mode)
-    [filesTestNames, imagesTest, labelsTest] = createSamplesDatastructures(listImagesNamesTest, interesting_labels, mode)
-
-
-    trainFeaturesFileName = 'trainFeatures_' + partialMd5CheckSum + '.b'
-    testFeaturesFileName =  'testFeatures_' + partialMd5CheckSum + '.b'
-
-    if not os.path.isfile(trainFeaturesFileName) or not os.path.isfile(testFeaturesFileName):
-
-    	#YOLO CNN uses images in range [0,1], while other models (VGG, faster RCNN ...) use images in range [0,255]
-    	if 'yolo' not in model_filename:
-    		imagesScale = 1.0;
-    	else:
-    		imagesScale = 255.0
-
-        transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-        transformer.set_transpose('data', (2,0,1)) #move image channels to outermost dimension 
-        transformer.set_raw_scale('data', imagesScale) 
-
-        if mode is 'imagenet' and 'yolo' not in model_filename:
-        	imagenetMeanPath = caffe.__path__[0] + '/imagenet/ilsvrc_2012_mean.npy'
-        	imagenetMean = np.load(imagenetMeanPath)
-        	imagenetMean = imagenetMean/(255.0/imagesScale)
-        	transformer.set_mean('data',imagenetMean.mean(1).mean(1))
-
-
-        #Update the sets of images by transforming them according to Transformer
-        for  index in range(len(imagesTrain)):
-            imagesTrain[index] = transformer.preprocess('data', imagesTrain[index])
-
-        for  index in range(len(imagesTest)):
-            imagesTest[index] = transformer.preprocess('data', imagesTest[index])		
-
-
-        #Forward the net on all images -> featureVectorsTrain featureVectorsTest
-        featureVectorsTrain = []
-        featureVectorsTest = []
-
-        # Extract the full feature vector from last fc layer once cropped images are forwarded to the net
-
-        t1 = time.time()
-        featureVectorsTrain = extractFeatures(imagesTrain, net, extractionLayerName)
-        featureVectorsTest = extractFeatures(imagesTest, net, extractionLayerName)
-        print 'Features extraction took ',(time.time() - t1) ,' seconds for ', (len(imagesTrain) + len(imagesTest)), ' images'
-
-        oldTrainFeaturesFile = glob.glob('trainFeatures_*')
-        oldTestFeaturesFile = glob.glob('testFeatures_*')
-
-        for file in oldTrainFeaturesFile:
-            os.remove(file)
-
-        for file in oldTestFeaturesFile:
-            os.remove(file)
-
-
-        #Dump features in 2 files -> trainFeatures testFeatures
-        with open(trainFeaturesFileName, 'wb') as trainFeaturesFile:
-            pickle.dump((filesTrainNames, featureVectorsTrain), trainFeaturesFile)
-
-        with open(testFeaturesFileName, 'wb') as testFeaturesFile:
-            pickle.dump((filesTestNames, featureVectorsTest), testFeaturesFile)	
-
-
-    else:
-
-        print 'Opening old features.... '
-
-        #Load features from a previously dumped file
-        with open(trainFeaturesFileName, 'rb') as trainFeaturesFile:
-            (filesTrainNames, featureVectorsTrain) = pickle.load(trainFeaturesFile)
-            featureVectorsTrain = np.array(featureVectorsTrain)
-
-        with open(testFeaturesFileName, 'rb') as testFeaturesFile:
-            (filesTestNames, featureVectorsTest) = pickle.load(testFeaturesFile)
-            featureVectorsTest = np.array(featureVectorsTest)
-
-
-
-
-    featureVectorsTrainNormalized = []
-    featureVectorsTestNormalized = []
-
-
-    for vec in featureVectorsTrain:
-        vecNormalized = vec/np.linalg.norm(vec)
-        featureVectorsTrainNormalized.append(vecNormalized)
-
-    for vec in featureVectorsTest:
-        vecNormalized = vec/np.linalg.norm(vec)
-        featureVectorsTestNormalized.append(vecNormalized)	
-
-    trainMean = np.mean(featureVectorsTrainNormalized, axis = 0)
-    testMean = np.mean(featureVectorsTestNormalized, axis = 0)	
-
-    featureVectorsTrainNormalizedCentered = []
-    featureVectorsTestNormalizedCentered = []
-
-    for vec in featureVectorsTrainNormalized:
-        vecCentered = vec - trainMean
-        featureVectorsTrainNormalizedCentered.append(vecCentered)
-
-    for vec in featureVectorsTestNormalized:
-        vecCentered = vec - testMean
-        featureVectorsTestNormalizedCentered.append(vecCentered)
-
-
-
-    #Train a multiclass SVM and a one-class SVM given all feature vectors
-    t1 = time.time()
-    modelSVM = svm.SVC(kernel="rbf", C=1e6, probability=True) # little regularization needed - get this training set right, neglect margin
-    modelSVM.fit(featureVectorsTrainNormalizedCentered, labelsTrain)
-    print 'SVM training took ',(time.time() - t1) ,' seconds'
-
-    t1 = time.time()
-    modelSVM_1 = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
-    modelSVM_1.fit(X_train)
-    print 'SVM_1 training took ',(time.time() - t1) ,' seconds'
-
-    countCorrect = 0
-
-    #Test the SVM using the extracted testFeatures
-    t1 = time.time()
-    prediction = []
-    for index in range(len(filesTestNames)):
-        features =  np.array(featureVectorsTestNormalizedCentered[index]).reshape((1, -1))
-        prediction.append(modelSVM.predict(features))
-        print 'For image ', filesTestNames[index], ' ... Predicted: ', prediction[-1], ' TrueLabel: ', labelsTest[index]
-        if prediction[-1] == labelsTest[index]:
-            countCorrect+=1
-    print 'SVM test took ',(time.time() - t1) ,' seconds'
-    print 'Accuracy: ', float(countCorrect)/len(labelsTest)
-
-    #confusion matrix
-    set_ = {}
-    map(set_.__setitem__, labelsTrain, [])
-    classes = list(set_.keys())
-    cnf_matrix = confusion_matrix(labelsTest, prediction)
-    plt.figure()
-    plot_confusion_matrix(cnf_matrix, classes=classes)
-    plt.show()
-
-
-
-
-    filters = net.params['conv1'][0].data
-    vis_square(filters.transpose(0, 2, 3, 1))
-
-
-    #activatedFeatures = net.blobs['conv1'].data #Not working on activation features....
-    #vis_square(activatedFeatures.transpose(0, 2, 3, 1))
+    [noveltySVM, multiclassSVM] = trainSVMsFromCroppedImages(net, extractionLayerName, images_dir, annotations_dir)
+   
 
 
 if __name__=='__main__':	
